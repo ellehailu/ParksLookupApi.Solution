7e1aeb116a34b99a470f7cedbb44cdb8720dd3ec
ellehailu <hailu.elle@gmail.com>
2023-08-18 10:08:56 -0700

    update appsettings.json, create a Park model and update program.cs

---
+++ ParksLookupApi/Models/Park.cs
@@ -0,0 +1,8 @@
+namespace ParksLookupApi.Models{
+    public class Park{
+        public int ParkId { get; set;}
+        public string Name { get; set;}
+        public string State { get; set;}
+        public string Designation { get; set;}
+    }
+}
