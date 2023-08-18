8b29eb3db3db3be2dbfa76ffb789826c68300d64
ellehailu <hailu.elle@gmail.com>
2023-08-18 13:33:53 -0700

    add get method to return all data point

--- ParksLookupApi/Controllers/ParksController.cs
+++ ParksLookupApi/Controllers/ParksController.cs
@@ -4,5 +4,20 @@ using ParksLookupApi.Models;
 
 namespace ParksLookupApi.Controllers
 {
+    [Route("api/[controller]")]
+    public class ParksController : Controller
+    {
+        private readonly ParksLookupApiContext _db;
 
+        public ParksController(ParksLookupApiContext db)
+        {
+            _db = db;
+        }
+
+        //Get api/Parks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Park>>> Get(){
+            return await _db.Parks.ToListAsync();
+        }
+    }
 }
