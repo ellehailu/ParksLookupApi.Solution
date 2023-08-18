2eec7a6646208f0db0c6ae1590ef5f5e9cdc4f4b
ellehailu <hailu.elle@gmail.com>
2023-08-18 10:16:36 -0700

    create a ParksLookupContext file and update the content

--- ParksLookupApi/Models/Park.cs
+++ ParksLookupApi/Models/Park.cs
@@ -1,8 +1,12 @@
-namespace ParksLookupApi.Models{
-    public class Park{
-        public int ParkId { get; set;}
-        public string Name { get; set;}
-        public string State { get; set;}
-        public string Designation { get; set;}
+namespace ParksLookupApi.Models
+{
+    public class Park
+    {
+        public int ParkId { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public string Designation { get; set; }
+
+        public DateTime DateOfEstablishment { get; set; }
     }
 }
